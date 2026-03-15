8349357c0460777150dde68baebaaae6397f2def
Faiq Prabaswara <faiqprabaswara1101@gmail.com>
2026-03-15 19:37:30 +0700

    menambahkan class DoorMachine

---
+++ TP_MODUL4_103022400130/DoorMachine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TP_MODUL4_103022400130
+{
+    internal class DoorMachine
+    {
+        enum State { Terkunci, Terbuka }
+
+        private State currentState;
+
+        public DoorMachine()
+        {
+            currentState = State.Terkunci;
+            Console.WriteLine("State awal: Terkunci");
+        }
+
+        public void BukaPintu()
+        {
+            if (currentState == State.Terkunci)
+            {
+                currentState = State.Terbuka;
+                Console.WriteLine("Pintu tidak terkunci");
+            }
+            else
+            {
+                Console.WriteLine("Pintu sudah terbuka");
+            }
+        }
+
+        public void KunciPintu()
+        {
+            if (currentState == State.Terbuka)
+            {
+                currentState = State.Terkunci;
+                Console.WriteLine("Pintu terkunci");
+            }
+            else
+            {
+                Console.WriteLine("Pintu sudah terkunci");
+            }
+        }
+    }
+}
