c3df2c4d5c73d3c399a23042d8b86832b5bbd4cd
Faiq Prabaswara <faiqprabaswara1101@gmail.com>
2026-03-15 19:32:22 +0700

    second commit

--- TP_MODUL4_103022400130/Program.cs
+++ TP_MODUL4_103022400130/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using TP_MODUL4_103022400130;
+
+namespace TP_MODUL4_NIM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KodePos kodePos = new KodePos();
+
+            Console.WriteLine("=== TABLE DRIVEN: KODE POS ===");
+            Console.WriteLine("Batununggal : " + kodePos.getKodePos("Batununggal"));
+            Console.WriteLine("Kujangsari  : " + kodePos.getKodePos("Kujangsari"));
+            Console.WriteLine("Mengger     : " + kodePos.getKodePos("Mengger"));
+            Console.WriteLine("Wates       : " + kodePos.getKodePos("Wates"));
+            Console.WriteLine("Cijaura     : " + kodePos.getKodePos("Cijaura"));
+            Console.WriteLine("Jatisari    : " + kodePos.getKodePos("Jatisari"));
+            Console.WriteLine("Margasari   : " + kodePos.getKodePos("Margasari"));
+            Console.WriteLine("Sekejati    : " + kodePos.getKodePos("Sekejati"));
+            Console.WriteLine("Kebonwaru   : " + kodePos.getKodePos("Kebonwaru"));
+            Console.WriteLine("Maleer      : " + kodePos.getKodePos("Maleer"));
+
+            Console.WriteLine();
+            Console.WriteLine("Tekan Enter untuk lanjut ke simulasi DoorMachine...");
+            Console.ReadLine();
+
+            DoorMachine door = new DoorMachine();
+
+            Console.WriteLine("=== STATE BASED CONSTRUCTION: DOOR MACHINE ===");
+            door.BukaPintu();
+            door.KunciPintu();
+            door.BukaPintu();
+        }
+    }
+}
--- TP_MODUL4_103022400130/obj/Debug/net10.0/TP_MODUL4_103022400130.AssemblyInfo.cs
+++ TP_MODUL4_103022400130/obj/Debug/net10.0/TP_MODUL4_103022400130.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL4_103022400130")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00b8a0b80557e2113c1d899712e60cc9daf01b85")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL4_103022400130")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL4_103022400130")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
